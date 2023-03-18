f7f7eb248c4a1560029757dd65f518d6c8890d1d
ezurno <ezurno@gmail.com>
2023-03-18 20:55:36 +0900

    UPDATE: create UI
    1. ui 를 먼저 만듦
    
    2. Theme.of 로 상위 widget 의 데이터 값을 받아와 style 을 적용
    
    3. Flexible 은 css 의 grid 와 비슷한 개념, 유연하게 화면분할을 해줌
    
    4. flex 는 Flexible 의 화면 분할 할당 값
    
    5. alignment 는 child 가 시작할 위치를 정해줌
    
    6. Expanded 는 container 를 화면 끝까지 늘려주는데에 사용하며 가로로 늘릴 땐 Row와 함께 써야 함

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,84 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            // UI를 기반으로 더 유연하게 해주는 Widget
+            flex: 1,
+            // flex 는 각 flexible 당 할당하는 화면 배율의 크기
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              // alignment 는 child 가 시작할 align의 위치를 정해줌 (하단 중앙에 시작)
+              child: Text(
+                "25:00",
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                color: Theme.of(context).cardColor,
+                iconSize: 100,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.play_circle_outline_rounded,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Jumodoros",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
